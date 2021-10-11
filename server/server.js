878e0e83407c281cae6083d632f49fd56b79d301
Jin Cheng <jincheng411@gmail.com>
2021-10-11 17:49:51 -0400

    set up webpack

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', (req, res) => {
   res.send('hello, world')
