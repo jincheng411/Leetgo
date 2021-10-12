3c2d5e5673fda5c34ad38ce5ec49c905d5c91ea6
Jin Cheng <jincheng411@gmail.com>
2021-10-12 02:05:57 -0400

    add routes for login logout and signup

--- server/route/user.js
+++ server/route/user.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const User = require('../models/user.js');
 
-router.post('/register', function (req, res) {
-  console.log('jinjinjinjin')
+router.post('/signup', function (req, res) {
   Users = new User({ email: req.body.email, username: req.body.username });
   User.register(Users, req.body.password, function (err, user) {
     if (err) {
@@ -22,5 +21,37 @@ router.post('/register', function (req, res) {
         })
     }
   });
+});
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, (err, person) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+      success: true,
+      status: 'You are successfully logged in!'
+    });
+  })
+})
+
+router.post('/logout', (req, res, next) => {
+  if (req.session) {
+    req.logout();
+    req.session.destroy((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.clearCookie('session-id');
+        res.json({
+          message: 'You are successfully logged out!'
+        });
+      }
+    });
+  } else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
 });
 module.exports = router;
